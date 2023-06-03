01237c81574c9ca81db60154a89e9a01261772cc
MrLimeick <apmew8899@gmail.com>
2023-06-04 00:52:57 +0300

    Обновление Nav-bar'а

--- NavBar.js
+++ NavBar.js
@@ -1,5 +1,29 @@
 let ToI = document.getElementById("ToI");
 ToI.onclick = function()
 {
-    alert("Да, пока-что только информатика… Пока-что!")
+    alert("Да, пока-что только информатика… Пока-что!");
+}
+
+let nav = document.getElementById("nav-bar");
+
+document.addEventListener("scroll", test);
+test();
+
+function test() 
+{
+    var bodyRect = document.body.getBoundingClientRect(),
+        elemRect = nav.getBoundingClientRect(),
+        offset   = elemRect.top - bodyRect.top;
+
+    if(offset > 20) 
+    {
+        let t = clamp((offset - 20) * 5);
+        console.log(t);
+        nav.style.boxShadow = "0px 0px " + t + "px 0px rgba(0, 0, 0, 0.5)";
+    }
+    else nav.style.boxShadow = "none";
+}
+
+function clamp(x) {
+    return Math.min(Math.max(x, 0), 100);
 }
