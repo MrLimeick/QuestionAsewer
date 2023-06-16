0d20cc0a2b6e21bdc6d7f028c07746c5ef5b9935
MrLimeick <apmew8899@gmail.com>
2023-06-16 19:04:52 +0300

    Обновления стиля
    Теперь сайт в ночном режиме(Возможно потом я добавлю переключение на светлый режим)
    Footer удалён (пока-что…)
    Nav bar теперь не прикреплён к вверху экрана(Я не думаю что нужно что-бы он был прикреплён…)

--- NavBar.js
+++ NavBar.js
@@ -1,18 +1,18 @@
-let nav = document.getElementById("nav-bar");
+// let nav = document.getElementById("nav-bar");
 
-document.addEventListener("scroll", NavShadow);
-NavShadow();
+// document.addEventListener("scroll", NavShadow);
+// NavShadow();
 
-function NavShadow() 
-{
-    var bodyRect = document.body.getBoundingClientRect(),
-        elemRect = nav.getBoundingClientRect(),
-        offset   = elemRect.top - bodyRect.top;
+// function NavShadow() 
+// {
+//     var bodyRect = document.body.getBoundingClientRect(),
+//         elemRect = nav.getBoundingClientRect(),
+//         offset   = elemRect.top - bodyRect.top;
 
-    let t = clamp((offset - 10) * 5);
-    nav.style.boxShadow = "0px 2px " + t + "px 0px rgb(180, 180, 180)";
-}
+//     let t = clamp((offset - 10) * 5);
+//     nav.style.boxShadow = "0px 2px " + t + "px 0px rgb(180, 180, 180)";
+// }
 
-function clamp(x) {
-    return Math.min(Math.max(x, 5), 50);
-}
+// function clamp(x) {
+//     return Math.min(Math.max(x, 5), 50);
+// }
